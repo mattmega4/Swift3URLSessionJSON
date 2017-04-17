dd07eebc5a8a4587f583c19703728181418b807c
Matthew Singleton <mattmega4@gmail.com>
2017-04-17 05:08:07 -0700

    first commit

---
+++ Swift3URLSessionJSON/Potter.swift
@@ -0,0 +1,9 @@
+//
+//  Potter.swift
+//  Swift3URLSessionJSON
+//
+//  Created by Matthew Singleton on 4/17/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import Foundation
---
+++ Swift3URLSessionJSON/PotterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PotterTableViewCell.swift
+//  Swift3URLSessionJSON
+//
+//  Created by Matthew Singleton on 4/17/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class PotterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
